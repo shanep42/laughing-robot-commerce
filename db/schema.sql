14b4196e3b13c9f0872fd881ca0c0626257b889c
Shane <shanep42@gmail.com>
2022-10-13 15:47:47 -0500

    Actually SAVES the newly created models

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,9 @@
+-- Active: 1664840661577@@127.0.0.1@3306@ecommerce_db
+
 -- DROP DATABASE
+
 DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
 
+CREATE DATABASE ecommerce_db;
