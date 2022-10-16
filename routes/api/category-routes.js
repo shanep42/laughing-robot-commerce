fdd6ddfcaee90c21b156cce551e73f84a1514beb
Shane <shanep42@gmail.com>
2022-10-16 14:22:34 -0500

    Adds GET one route to all Models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,9 +16,17 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // be sure to include its associated Products
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
