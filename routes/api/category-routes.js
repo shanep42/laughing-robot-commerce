56584c04f499ae9a1e92e7c0517b708bbb4c8b34
Shane <shanep42@gmail.com>
2022-10-17 17:00:31 -0500

    Adds Post route to categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,7 +31,14 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-});
+  Category.create(req.body)
+    .then((category) => {
+      res.status(200).json(category)
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    })
+})
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
@@ -39,6 +46,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: 'No category found with this id' })
+      }
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;
